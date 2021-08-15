49161c8045a21a10d944aa87c7e6eac7b97e813d
kotaro_sakae <ksakae1216@gmail.com>
2021-08-15 10:41:53 +0900

    Add typescript

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # AtCoder-tutorial-javascript
 
+## 準備
+
+```
+npm i
+yarn install
+```
+
+
 ## practice-a
 実行方法
+```
 node practice-a.js
+```
+or
+```
+npx ts-node a.ts
+```
---
+++ Shift-only/a.ts
@@ -0,0 +1,29 @@
+import * as fs from 'fs';
+
+let cnt = 0;
+
+const main = (input: any) => {
+
+	const tmp = input.split('\n');
+	var a = tmp[0];
+	var b = tmp[1].split(' ');
+
+  waru(b);
+
+  console.log(cnt);
+}
+
+function waru(valList: any) {
+  for(let i=0; i<valList.length; i++) {
+    if((valList[i] % 2) === 1) return;
+
+    valList[i] = valList[i] / 2;
+  }
+  cnt += 1;
+
+  waru(valList);
+}
+
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+main(require("fs").readFileSync("./input.txt", "utf8"));
+// Main(require("fs").readFileSync("/dev/stdin", "utf8"));
